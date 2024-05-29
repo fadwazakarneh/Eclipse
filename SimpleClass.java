89829643a06450f35cf71feb5e9cd826d4651a79
fadwazakarneh <fadwa.zakarneh21@gmail.com>
2024-05-29 13:58:59 +0300

    new version

--- SimpleClass.java
+++ SimpleClass.java
@@ -1,4 +1,5 @@
 package myapp;
+/newline
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
--- run_firefox.java
+++ run_firefox.java
@@ -1,5 +1,5 @@
 package myapp;
-
+//newline
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;

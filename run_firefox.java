315e32a16be875a8e6a52bae281e277cb4312e4a
fadwazakarneh <fadwa.zakarneh21@gmail.com>
2024-05-29 14:11:49 +0300

    new version 2

--- run_firefox.java
+++ run_firefox.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
-
+//nddd
 public class run_firefox{
 
     public static void main(String[] args) {
